706fb7330dcc749d7bffae6c9010f31735cc0838
tamar-revazishvili <92332147+tamar-revazishvili@users.noreply.github.com>
2021-11-19 14:11:22 +0200

    Update Ex1.py
    final update: added main with args

--- Ex1.py
+++ Ex1.py
@@ -1,25 +1,29 @@
 import csv
 from Building import Building
 from Calls import Calls
+import sys
+
 
 class Ex1:
-    def __init__(self, buildingJson, callsCsv) -> None:  ## , output) :
+    def __init__(self, buildingJson, callsCsv, output):
+        self.buildingJson = buildingJson
+        self.callsCsv = callsCsv
+        self.output = output
         self.b = Building()
         self.c = Calls()
         self.b.from_json(buildingJson)
         self.e = self.b.elevators
         self.c.from_csv(callsCsv)
         self.assignedElev = []
+
         for i in self.c.elevCalls:
             self.assignedElev.append(self.assignElev(self.b, i))
-        # for i in self.assignedElev:
-        #     print(i)
-        self.save_csv(self.assignedElev, callsCsv)
+        self.save_csv(callsCsv)
 
     def assignElev(self, b, call):
         min = self.calculateTime(b.lastFloor[0], b.elevators[0], call)
-        elev = b.elevators[0]._id  #
-        b.lastFloor[elev] = call[2]  #
+        elev = b.elevators[0]._id
+        b.lastFloor[elev] = call[2]
         elev = 0
         for i in b.elevators:
             temp = self.calculateTime(b.lastFloor[i], b.elevators[i], call)
@@ -27,16 +31,14 @@ class Ex1:
                 min = temp
                 elev = b.elevators[i]._id
                 b.lastFloor[elev] = call[2]
-        # print(b.lastFloor)
         return elev
 
-    # CloseTime + StartTime + df/speed + StopTime + OpenTime (df == |floor_a-floor_b|).
     def calculateTime(self, lastFloor, elev, call):
         df = abs(call[2] - call[1])
         return elev._closeTime + elev._startTime + (df / elev._speed) + elev._stopTime + elev._openTime + abs(
             lastFloor - call[1])
 
-    def save_csv(self, assignedElevList, file_name):
+    def save_csv(self, file_name):
         try:
             with open(file_name, "r") as r:
                 r = csv.reader(r)
@@ -45,7 +47,7 @@ class Ex1:
                 for i in lines:
                     i[5] = self.assignedElev[index]
                     index += 1
-            with open("output.csv", "w", newline='') as w:
+            with open(self.output, "w", newline='') as w:
                 w = csv.writer(w)
                 w.writerows(lines)
         except IOError as e:
@@ -53,11 +55,6 @@ class Ex1:
 
 
 if __name__ == '__main__':
-    #     # b = Building()
-    #     # b.from_json("B1.json")
-    #     # print(b)
-    #     # c = Calls()
-    #     # c.from_csv("Calls_a.csv")
-    #     # print(c)
-    ans = Ex1("B5.json", "Calls_a.csv")
-    # print(ans)
+    n = len(sys.argv)
+    if n == 0 or n <= 4:
+        ans = Ex1(sys.argv[1], sys.argv[2], sys.argv[3])
